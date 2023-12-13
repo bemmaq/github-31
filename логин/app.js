142782a084de38c0fcacee3b9a9d15f785dde327
bemmaq <152206205+bemmaq@users.noreply.github.com>
2023-12-13 08:52:37 +0300

    Add files via upload

---
+++ логин/app.js
@@ -0,0 +1,37 @@
+function validateForm(){
+    const username=document.getElementById('username').value;
+    const email=document.getElementById('email').value;
+    const password=document.getElementById('password').value;
+    const password2=document.getElementById('password2').value;
+
+    document.getElementById("name-error").innerHTML="";
+    document.getElementById("email-error").innerHTML="";
+    document.getElementById("password-error").innerHTML="";
+    document.getElementById("password2-error").innerHTML="";
+
+    let isValid=true;
+
+    if(username === ""){
+        document.getElementById("name-error").innerHTML="*Name tidak boleh kosong";
+        isValid=false;
+    }
+
+    
+    if(email === ""){
+        document.getElementById("email-error").innerHTML="*Email tidak boleh kosong";
+        isValid=false;
+    }
+
+    
+    if(password === ""){
+        document.getElementById("password-error").innerHTML="*Password tidak boleh kosong";
+        isValid=false;
+    }
+
+    
+    if(password !== password2){
+        document.getElementById("password2-error").innerHTML="*Passwords tidak boleh kosong";
+        isValid=false;
+    }
+    return isValid
+}
